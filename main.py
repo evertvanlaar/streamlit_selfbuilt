c7741d4895ccac0d63588cbe2a674dcd26d3185a
evertvanlaar <evert.van.laar@gmail.com>
2023-04-12 16:00:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import streamlit as st
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
+# ---- LOAD ASSETS ----
+lottie_coding = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_fcfjwiyb.json")
+img_contact_form = Image.open("images/yt_contact_form.png")
+img_lottie_animation = Image.open("images/yt_lottie_animation.png")
+
 # ---- HEADER SECTION ----
 with st.container():
     st.subheader("Hi, I am Evert :wave:")
